c9c66b4859038918725e6f9022b342475c40e50d
jluzadasjr <jayrl707@Jesuss-MacBook-Pro.local>
2019-03-19 21:58:33 -0700

    updated code

---
+++ app/data/friends.js
@@ -0,0 +1,104 @@
+var friends = [
+{
+    name:"Peter Griffin",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    score:[
+        5,
+        3,
+        2,
+        2,
+        1,
+        5,
+        3,
+        1,
+        2,
+        1
+      ]
+  },
+
+  {
+    name:"Lois Griffin",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    score:[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+
+  {
+    name:"Stewie Griffin",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    score:[
+        1,
+        5,
+        1,
+        3,
+        4,
+        1,
+        5,
+        5,
+        5,
+        2
+      ]
+  },
+
+  {
+    name:"Chris Griffin",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    score:[
+        5,
+        5,
+        5,
+        5,
+        5,
+        1,
+        5,
+        5,
+        4,
+        5
+      ]
+  },
+
+  {
+    name:"Glenn Quagmire",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    score:[
+        3,
+        4,
+        4,
+        5,
+        2,
+        3,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+
+  {
+    name:"Brian",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    score:[
+        3,
+        4,
+        4,
+        3,
+        5,
+        5,
+        2,
+        5,
+        3,
+        2
+      ]
+  }
+];
+module.exports = friends;
