691e09a4033cc960a81c6b72d1ce21df076581e8
jluzadasjr <jayrl707@calvisitor-10-105-129-91.calvisitor.1918.berkeley.edu>
2019-03-21 18:22:49 -0700

    updated code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -29,17 +29,25 @@ module.exports = function(app) {
     // ---------------------------------------------------------------------------
   
     app.post("/api/friends", function(req, res) {
+      //  get total user score 
+      //get total use score for each friend
+      //compare user score with friend's score
+      //find friend with least difference 
+      //once you have friend, send friend name and photo to front end.
+
+      console.log(req.body);
       // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
       // It will do this by sending out the value "true" have a table
       // req.body is available since we're using the body parsing middleware
-      if (friendsData.length < 5) {
-        friendsData.push(req.body);
-        res.json(true);
-      }
-      else {
-        friendsData.push(req.body);
-        res.json(false);
-      }
+      // if (friendsData.length < 5) {
+      //   friendsData.push(req.body);
+      //   res.json(true);
+      // }
+      // else {
+      //   friendsData.push(req.body);
+      //   res.json(false);
+      // }
+      res.send("random string");
     });
   
     // ---------------------------------------------------------------------------
